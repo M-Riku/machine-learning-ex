8f935d96eeb6320c88a6fb7232dfdaac0ac80175
M-Riku <luminghao1994@yahoo.co.jp>
2019-09-26 10:15:42 +0900

    submit ex6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+test_data = [0.01 0.03 0.1 0.3 1 3 10 30];
+l = length(test_data);
+best_error = 100000000;
+for i = 1:l
+    for j = 1:l
+        C_temp = test_data(i);
+        sigma_temp = test_data(j);
+        model= svmTrain(X, y, C_temp, @(x1, x2) gaussianKernel(x1, x2, sigma_temp));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        if error < best_error
+            best_error = error;
+            C = C_temp;
+            sigma = sigma_temp;
+        end;
+    end;
+end;
 
 % =========================================================================
 
